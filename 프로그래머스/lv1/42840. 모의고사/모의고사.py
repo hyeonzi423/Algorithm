396bb0b958f1f91a8c4316db85398b8634db2858
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-04 11:41:20 +0900

    [level 1] Title: 모의고사, Time: 1.94 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,18 +1,9 @@
-def copy_s(n, m):
-    num = 1 if n / m < 1 else int(n / m)
-    return num
-
-
 def solution(answers):
     answer = []
     c_1, c_2, c_3 = 0, 0, 0 
     s_1 = [1,2,3,4,5] * int(10000/5)
     s_2 = [2,1,2,3,2,4,2,5] * int(10000/8)
     s_3 = [3,3,1,1,2,2,4,4,5,5] * int(10000/10)
-    
-    # s_1_answers = s_1 * copy_s(len(answers), 5)
-    # s_2_answers = s_2 * copy_s(len(answers), 8)
-    # s_3_answers = s_3 * copy_s(len(answers), 10)
 
     for i in range(len(answers)):
         if answers[i] == s_1[i]:
