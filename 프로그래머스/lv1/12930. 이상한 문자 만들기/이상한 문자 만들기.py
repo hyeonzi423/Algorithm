f737b809fbc2554398052c6600d79760321efaad
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-07 00:26:20 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ""
+    tmp =  list(map(str, s.split(" ")))
+    for i in range(len(tmp)):
+        if tmp[i] == " ":
+            answer += " " 
+        for j in range(len(tmp[i])):
+            if j % 2 == 0:
+                answer += str(tmp[i][j].upper())
+            else:
+                answer += str(tmp[i][j].lower())
+        if i < len(tmp) - 1:
+            answer += " "
+    return answer
