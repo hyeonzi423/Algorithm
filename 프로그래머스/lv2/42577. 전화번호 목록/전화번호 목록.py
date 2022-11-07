524a2ff2958713571bae905f17c5fa5fc4d13fea
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-07 18:51:04 +0900

    [level 2] Title: 전화번호 목록, Time: 101.61 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    phone_book.sort()
+    answer = True
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+            answer = False
+            break
+    return answer
+
+print(solution(["934793", "34", "44", "9347"]))
