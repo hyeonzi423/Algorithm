dde29e22413e48dc0b0cc12136f11c0367683769
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-08 23:25:02 +0900

    [level 2] Title: 숫자의 표현, Time: 3.69 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,11 +1,10 @@
-def solution(n):
-    n_prime = [n]
+def solution(num):
     answer = 0
-    for i in range(1, int(n**1/2) + 1):
-        if n % i == 0:
-            n_prime.append(i)
-                
-    for j in range(len(n_prime)):
-        if n_prime[j] % 2 != 0:
+    for i in range(1, num + 1):
+        s = 0
+        while s < num:
+            s += i
+            i += 1
+        if s == num:
             answer += 1
     return answer
