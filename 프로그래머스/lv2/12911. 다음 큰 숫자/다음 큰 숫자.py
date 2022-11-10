7253cfd284bd8a7e258a05e25acd98002a3548d8
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-10 12:37:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,8 +1,7 @@
 def solution(n):
-    n_1 = (bin(n)[2:]).count("1")
-    
+    n_1 = format(n, "b").count("1")
     for i in range(n + 1, 1000000):
-        new = (bin(i)[2:]).count("1")
+        new = format(i, "b").count("1")
         if new == n_1:
             break
     answer = i
