b2579df6fd6f0bea6cbfd765add6c23a7f4ebb2c
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-10 14:48:40 +0900

    [level 2] Title: 피보나치 수, Time: 6.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,6 @@
 def solution(n):
     f0, f1 = 0, 1
-    for i in range(2, n+1):
+    for _ in range(2, n+1):
         f_n = (f0 + f1) % 1234567
-        f0 = f1
-        f1 = f_n
+        f0, f1 = f1, f_n
     return f_n
