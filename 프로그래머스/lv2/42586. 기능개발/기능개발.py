8c135e6a9b81261e4174dfa174ca253ba81a839e
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-11 18:51:48 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,21 @@
+def solution(progresses, speeds):
+    r_days, res = [], []
+    for p, s in zip(progresses, speeds):
+        day = (100 - p) // s if (100 - p) % s == 0 else (100 - p) // s + 1 
+        r_days.append(day)
+
+    tmp_1 = r_days.pop(0)
+    cnt = 1
+    while len(r_days) > 0:
+        tmp_2 = r_days.pop(0)
+        if tmp_2 > tmp_1:
+            res.append(cnt)
+            tmp_1 = tmp_2
+            cnt = 1
+            if len(r_days) == 0:
+                res.append(cnt)
+        else:
+            cnt += 1
+            if len(r_days) == 0:
+                res.append(cnt)
+    return res 
