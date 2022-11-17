662faad191d544e56540d9fd198334f5c8c41cd3
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-17 10:39:57 +0900

    [level 2] Title: H-Index, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,11 +1,8 @@
 def solution(citations):
-    tmp = []
-    citations.sort(reverse = True)
+    answer = 0
     for i in range(len(citations) + 1):
         num1 = [a for a in citations if a >= i]
         num2 = [b for b in citations if b <= i]
-        print(len(num1), i, len(citations) - len(num1), len(num2))
         if len(num1) >= i and len(num2) <= i:
-            tmp.append(i)
-    answer = max(tmp) if len(tmp) != 0 else 0
+            answer = i
     return answer
--- 프로그래머스/lv2/42747. H－Index/README.md
+++ 프로그래머스/lv2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
