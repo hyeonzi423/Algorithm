fc94c9e7862108fdd7253ce517447ecc70077433
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-21 15:34:24 +0900

    [level 2] Title: 튜플, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,26 @@
+def change(s):
+    s = [i for i in s[1: len(s) - 1]]
+    tmp1 = []
+    
+    for i in range(len(s)):
+        if s[i] == "{":
+            tmp2 = []
+        elif s[i] in ",}":
+            if len(tmp2) != 0:
+                tmp1.append("".join(tmp2))
+                tmp2 = []
+        else:
+            tmp2.append(s[i])
+    return tmp1
+
+def solution(s):
+    list_s = change(s)
+    set_s = set(list_s)  
+    count, answer = [], []
+    for i in set_s:
+        count.append(list_s.count(i))
+    dic_cnt = {int(i):list_s.count(i) for i in set_s}
+    dic_cnt = sorted(dic_cnt.items(), reverse = True, key = lambda item: item[1])
+    for i in dic_cnt:
+        answer.append(i[0])
+    return answer
