03c55bf05a86fa5f4c81567a987f21b94f21b87c
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-26 02:01:46 +0900

    [level 2] Title: 오픈채팅방, Time: 178.44 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,19 @@
+def solution(record):
+    record_l, answer, uid = [], [], []
+    for i in record:
+        tmp = list(map(str, i.split(" ")))
+        record_l.append(tmp)
+        uid.append(tmp[1])
+
+    dic_io = {user_id: [] for user_id in set(uid)}
+    for r in record_l:
+        if r[0] != "Leave":
+            dic_io[r[1]].append(r[2])
+    dic_io = {a: b[-1] for a, b in dic_io.items()}
+    
+    for i in record_l:
+        if i[0] == "Enter":
+            answer.append(dic_io[i[1]] + "님이 들어왔습니다.")
+        elif i[0] == "Leave":
+            answer.append(dic_io[i[1]] + "님이 나갔습니다.")  
+    return answer
