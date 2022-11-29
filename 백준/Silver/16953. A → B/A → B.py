f1bb01ebbd7624745e96c17b8f80f9cde08394b6
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-11-29 16:37:39 +0900

    [Silver II] Title: A → B, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+cnt = 0
+
+while b != a:
+    tmp = b
+    if b % 2 == 0:
+        b //= 2
+        cnt += 1
+    elif b % 10 == 1:
+        b //= 10
+        cnt += 1
+    if b <= a or tmp == b:
+        break
+print(cnt + 1 if b == a else -1)
