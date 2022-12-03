f9fcf33e831659987b1860cc9e6d8744f44efeff
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-12-03 22:22:43 +0900

    [level 2] Title: 방문 길이, Time: 1.30 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,18 @@
+def solution(dirs):
+    x, y, res = 0, 0, []
+    nx, ny = 0, 0
+    for i in dirs:
+        if i == 'U' and ny < 5:
+            ny += 1
+        elif i == 'D' and ny > -5:
+            ny -= 1
+        elif i == 'R' and nx < 5:
+            nx += 1
+        elif i == 'L' and nx > -5:
+            nx -= 1
+        if (x == nx and y == ny) == False:    
+            if (x, y, nx, ny) not in res:
+                if (nx, ny, x, y) not in res:                
+                    res.append((x, y, nx, ny))
+        x, y = nx, ny
+    return len(res)
