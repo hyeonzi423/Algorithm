797f29c99e14184f7c64fc1dc660169e7afe3235
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-12-09 14:26:19 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1404 ms, Memory: 32436 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,20 @@
+dp = {}
+def w(a, b, c):
+    if (a, b, c) in dp:
+        return dp[(a, b, c)]
+    else:
+        if a <= 0 or b <= 0 or c <= 0:
+            dp[(a, b, c)] = 1
+        elif a > 20 or b > 20 or c > 20:
+            dp[(a, b, c)] = w(20, 20, 20)
+        elif a < b < c:
+            dp[(a, b, c)] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        else:
+            dp[(a, b, c)] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return dp[(a, b, c)]
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == -1 and b == -1 and c == -1:
+        break
+    print("w(%d, %d, %d) = %d" % (a, b, c, w(a, b, c)))
