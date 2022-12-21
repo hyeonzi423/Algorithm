989ee47f53e9d3dac1f2409c858b7bb84e9cf31d
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-12-21 15:29:32 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -0,0 +1,12 @@
+def solution(dots):
+    answer = 0
+    for i in range(4):
+        for j in range(i+1, 4):
+            slide1 = (dots[j][1] - dots[i][1]) / (dots[j][0] - dots[i][0])
+            remain = [0,1,2,3]
+            remain.remove(i)
+            remain.remove(j)
+            slide2 = (dots[remain[1]][1] - dots[remain[0]][1]) / (dots[remain[1]][0] - dots[remain[0]][0])
+            if slide1 == slide2:
+                answer += 1
+    return 1 if answer != 0 else 0
