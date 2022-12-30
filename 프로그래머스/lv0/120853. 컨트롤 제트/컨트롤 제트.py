d6ac1c3798b7600f8dc118066a8f8033cb16acbd
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2022-12-30 19:01:06 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,11 +1,9 @@
 def solution(s):
-    answer, total = [], 0
+    total = 0
     s_l = s.split()
-    for i in range(len(s_l)-1):
-        if s_l[i+1] != "Z" and s_l[i] != "Z" :
-            answer.append(s_l[i])
-    if s_l[-1] != 'Z':
-        answer.append(s_l[-1])
-    for i in answer:
-        total += int(i)
+    for i in range(len(s_l)):
+        if s_l[i] != 'Z':
+            total += int(s_l[i])
+        else:
+            total -= int(s_l[i-1])
     return total
