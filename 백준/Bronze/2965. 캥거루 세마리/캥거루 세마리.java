85643087ae5db8b24a614c229c707ac9181d98c3
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2023-01-16 18:23:00 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 124 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int answer = c-b < b-a ? b-a-1 : c-b-1;
+        System.out.println(answer);
+    }
+}
