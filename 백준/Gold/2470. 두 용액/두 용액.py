493c8a76fb63aa519d5173a12476c1f0b0991099
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2023-02-09 12:32:09 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 42172 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,16 @@
+n = int(input())
+value = sorted(list(map(int, input().split())))
+left, right = 0, n - 1
+check, res1, res2 = abs(value[left] + value[right]), value[left], value[right]
+
+while left < right:
+    tmp = value[left] + value[right]
+    if abs(tmp) < check:
+        check = abs(tmp)
+        res1 = value[left]
+        res2 = value[right]
+    if tmp > 0:
+        right -= 1
+    else:
+        left += 1
+print(res1, res2)
