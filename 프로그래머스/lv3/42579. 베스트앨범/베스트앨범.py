85808cd818854e838b21655f056e3505ea945d95
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2023-09-07 01:05:29 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,17 +1,17 @@
 def solution(genres, plays):
     answer = []
-    music = {i: [] for i in set(genres)}
-    music_cnt = {i: 0 for i in set(genres)}
+    g = {i: [] for i in set(genres)}
+    g_n = {i: 0 for i in set(genres)}
     
-    for i in range(len(genres)):
-        music[genres[i]].append([plays[i], i])
-        music_cnt[genres[i]] += plays[i]
+    for i in range(len(plays)):
+        g[genres[i]].append([i, plays[i]])
+        g_n[genres[i]] += plays[i]
         
-    d_sort = dict(sorted(music_cnt.items(), key = lambda x: x[1], reverse = True))
-    
-    for i in d_sort:
-        tmp = music[i]
-        tmp.sort(key = lambda x:(-x[0], x[1]))
-        for j in tmp[:2]:
-            answer.append(j[1])
+    g_n = sorted(g_n.items(), key=lambda x: x [1], reverse = True)
+
+    for a, b in g_n:
+        tmp = g[a]
+        tmp.sort(key = lambda x: (-x[1], x[0]))
+        for i in tmp[:2]:
+            answer.append(i[0])
     return answer
