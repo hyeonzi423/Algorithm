87c5df1046044a9d2ca53a7a275c75f190a16f5d
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2023-09-12 22:15:10 +0900

    [level 2] Title: 주식가격, Time: 122.59 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,12 +1,13 @@
 def solution(prices):
-    res = []
-    for i in range(len(prices)-1): # 맨 마지막은 어차피 0 이기 때문에 길이 -1 까지만 반복
-        tmp = prices[i]
-        cnt = 0
+    answer = []
+    for i in range(len(prices) - 1):
+        now = prices[i]
+        cnt = 1
         for j in range(i+1, len(prices)):
-            cnt += 1 # tmp 바로 다음 수가 작아도 1초는 유지했다고 표현하기 때문에 cnt를 먼저 올림
-            if tmp > prices[j]: # 현재 tmp보다 큰 수를 찾으면 break 
+            if now > prices[j] or j == len(prices) - 1:
+                answer.append(cnt)
                 break
-        res.append(cnt)
-    res.append(0) # 맨 마지막에 0 추가
-    return res
+            else:
+                cnt += 1
+    answer.append(0)
+    return answer
