d9c8df5c6482658065f4cba0c6e63c3d29b251fa
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2023-10-21 01:04:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.50 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,15 @@
 from collections import deque
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    visited = [[0] * m for _ in range(n)]
-    dis = [[0] * m for _ in range(n)]
-    dq = deque()
-    dq.append([0, 0])
-    visited[0][0] = 1
-    dis[0][0] = 1
-
-    while dq:
-        x, y = dq.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and maps[nx][ny] == 1:
-                visited[nx][ny] = 1
-                dis[nx][ny] = dis[x][y] + 1
-                dq.append([nx, ny])
-
-    ans = dis[n-1][m-1] if dis[n-1][m-1] != 0 else -1
-    return ans
-
+    dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+    q = deque()
+    q.append([0, 0])
+    visited = [[1]*len(maps[0]) for _ in range(len(maps))]
+    while q:
+        x, y = q.popleft()
+        for f in range(4):
+            nx, ny = x + dx[f], y + dy[f]
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and visited[nx][ny] == 1 and maps[nx][ny] == 1:
+                visited[nx][ny] = visited[x][y] + 1
+                q.append([nx, ny])
+    answer = visited[-1][-1] if visited[-1][-1] != 1 else -1                
+    return answer
