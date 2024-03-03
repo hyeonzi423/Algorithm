19d38e33ada983f3d6d2d3d1733ad25701270db2
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-03-03 22:44:21 +0900

    [D4] Title: 염라대왕의 이름 정렬, Time: 802 ms, Memory: 143,764 KB -BaekjoonHub

---
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/README.md
@@ -0,0 +1,15 @@
+# [D4] 염라대왕의 이름 정렬 - 7701 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWqU0zh6rssDFARG) 
+
+### 성능 요약
+
+메모리: 143,764 KB, 시간: 802 ms, 코드길이: 1,178 Bytes
+
+### 제출 일자
+
+2024-03-03 22:44
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());			
+			TreeSet<String> set = new TreeSet<>(new Comparator<String>() {
+				@Override
+				public int compare(String o1, String o2) {
+					if(o1.length() != o2.length()) {
+						return o1.length() - o2.length();
+					}else {
+						for(int i = 0; i < o1.length(); i++) {
+							if(o1.charAt(i) != o2.charAt(i)) {
+								return o1.charAt(i) - o2.charAt(i);
+							}
+						}
+					}
+					return 0;
+				}
+			});
+			for(int i = 0; i < N; i++) {
+				String str = br.readLine();
+				if(!set.contains(str)) set.add(str);
+			}
+			
+			sb.append("#").append(tc).append("\n");
+			int s = set.size();
+			for(int i = 0; i < s; i++) {
+				sb.append(set.pollFirst()).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
