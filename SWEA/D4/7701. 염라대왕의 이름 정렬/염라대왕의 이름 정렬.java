1215d964e0f2880ca509746ba3501d7fbe387df6
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-03-03 22:45:53 +0900

    [D4] Title: 염라대왕의 이름 정렬, Time: 802 ms, Memory: 143,764 KB -BaekjoonHub

--- SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.java
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.java
@@ -6,21 +6,21 @@ import java.util.TreeSet;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());			
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
 			TreeSet<String> set = new TreeSet<>(new Comparator<String>() {
 				@Override
 				public int compare(String o1, String o2) {
-					if(o1.length() != o2.length()) {
+					if (o1.length() != o2.length()) {
 						return o1.length() - o2.length();
-					}else {
-						for(int i = 0; i < o1.length(); i++) {
-							if(o1.charAt(i) != o2.charAt(i)) {
+					} else {
+						for (int i = 0; i < o1.length(); i++) {
+							if (o1.charAt(i) != o2.charAt(i)) {
 								return o1.charAt(i) - o2.charAt(i);
 							}
 						}
@@ -28,14 +28,14 @@ public class Solution {
 					return 0;
 				}
 			});
-			for(int i = 0; i < N; i++) {
+			for (int i = 0; i < N; i++) {
 				String str = br.readLine();
-				if(!set.contains(str)) set.add(str);
+				if (!set.contains(str)) set.add(str);
 			}
-			
+
 			sb.append("#").append(tc).append("\n");
 			int s = set.size();
-			for(int i = 0; i < s; i++) {
+			for (int i = 0; i < s; i++) {
 				sb.append(set.pollFirst()).append("\n");
 			}
 		}
