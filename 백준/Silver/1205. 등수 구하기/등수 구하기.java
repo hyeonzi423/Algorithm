50de015b27d1a62c334926db5b27b7c758ae3811
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-11-22 09:23:58 +0900

    [Silver IV] Title: 등수 구하기, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,42 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		if (N == 0) {
-			System.out.println(1);
-			System.exit(0);
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        Long T = Long.parseLong(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int front = 0, rank = 0;
 
-		}
-		
-		int score = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-
-		int[] list = new int[N];
-		int rank = 1;
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		if (N == P && score <= list[list.length - 1])
-			System.out.println(-1);
-		
-		else {
-			for (int i = 0; i < list.length; i++) {
-				if (score < list[i])
-					rank++;
-				else
-					break;
-			}
-			System.out.println(rank);
-		}
-
-	}
+        if(N != 0){ 
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++){
+                Long tmp = Long.parseLong(st.nextToken());
+                if(tmp >= T){
+                    front++;
+                }if(tmp > T){
+                    rank++;
+                }
+            }
+        }
+        if(N == 0){
+            System.out.println(1);   
+        }else{
+            System.out.println(front == P ? -1 : rank + 1);   
+        }
+    }
 }
