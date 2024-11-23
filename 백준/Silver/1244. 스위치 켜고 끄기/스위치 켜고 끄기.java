495bae6f9ae9b4397d5b9d5d19ca4c4b9ee6cf72
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-11-23 20:36:33 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 120 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -3,53 +3,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		int[] light = new int[N+1];
+		int[] light = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			light[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int student = Integer.parseInt(br.readLine());
-		int gender, num;
-		for(int i = 0; i < student; i++) {
+
+		int O = Integer.parseInt(br.readLine());
+		for (int i = 0; i < O; i++) {
 			st = new StringTokenizer(br.readLine());
-			gender = Integer.parseInt(st.nextToken());
-			if(gender == 1) {
-				num =  Integer.parseInt(st.nextToken());
-				for(int j = 1; j <= N; j++) {
-					if(j%num == 0) {
-						light[j] = (light[j] + 1)%2;
-					}
+			int flag = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+
+			if (flag == 1) {
+				for (int j = 1; j <= N / num; j++) {
+					light[j * num - 1] = light[j * num - 1] == 0 ? 1 : 0;
 				}
-			}else {
-				int check = 0;
-				num =  Integer.parseInt(st.nextToken());
-				light[num] = (light[num] + 1)%2;
-				for(int j = 1; j < N/2; j++) {
-					if(num - j > 0 && num + j <= N) {
-						if(light[num - j] == light[num + j]) {
-							light[num - j] = (light[num - j] + 1)%2;
-							light[num + j] = (light[num + j] + 1)%2;
-						}else {
-							check = 1;
-						}
-					}
-					if(check == 1) {
+			} else {
+				num -= 1;
+				int c = 1;
+				light[num] = light[num] == 0 ? 1 : 0;
+				while (true) {
+					if (num - c >= 0 && num + c < N && light[num - c] == light[num + c]) {
+						light[num - c] = light[num - c] == 0 ? 1 : 0;
+						light[num + c] = light[num + c] == 0 ? 1 : 0;
+						c++;
+					}else {
 						break;
 					}
 				}
-				
 			}
 		}
 		int cnt = 0;
-		for(int i = 1; i <= N; i++) {
+		for(int i = 0; i < N; i++) {
 			System.out.print(light[i] + " ");
-			cnt += 1;
+			cnt++;
 			if(cnt == 20) {
 				System.out.println();
 				cnt = 0;
