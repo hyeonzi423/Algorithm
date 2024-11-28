4b253a3b7aa0b6418c6330db1f5d342bf06b785b
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-11-28 21:29:57 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 608 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,50 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
+	public static class Word implements Comparable<Word> {
+		String word;
+		int len, cnt;
+
+		public Word(String word, int len, int cnt) {
+			this.word = word;
+			this.len = len;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Word o) {
+			if (this.cnt != o.cnt) {
+				return Integer.compare(o.cnt, this.cnt);
+			} else if (this.len != o.len) {
+				return Integer.compare(o.len, this.len);
+			}
+			return this.word.compareTo(o.word);
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		Map<String, Integer> map = new HashMap<>();
-
-		for (int i = 0; i < n; i++) {
-			String s = br.readLine();
-			if (s.length() < m)
-				continue;
-			map.put(s, map.getOrDefault(s, 0) + 1);
+		HashMap<String, Integer> set = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			String w = br.readLine();
+			if (w.length() >= M) {
+				if (set.containsKey(w)) {
+					set.replace(w, set.get(w) + 1);
+				} else {
+					set.put(w, 0);
+				}
+			}
 		}
 
-		List<String> words = new ArrayList<>(map.keySet());
-
-		Collections.sort(words, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				if (Integer.compare(map.get(o1), map.get(o2)) != 0) {
-					return Integer.compare(map.get(o2), map.get(o1));
-				}
-				if (o1.length() != o2.length()) {
-					return o2.length() - o1.length();
-				}
-				return o1.compareTo(o2);
-			}
-		});
+		PriorityQueue<Word> pq = new PriorityQueue<>();
+		for (String w : set.keySet()) {
+			pq.add(new Word(w, w.length(), set.get(w)));
+		}
 
 		StringBuilder sb = new StringBuilder();
-		for (String str : words) {
-			sb.append(str + "\n");
+		while (!pq.isEmpty()) {
+			sb.append(pq.poll().word).append("\n");
 		}
+		
 		System.out.println(sb);
 	}
 }
