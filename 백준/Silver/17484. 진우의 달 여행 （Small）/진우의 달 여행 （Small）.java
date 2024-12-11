6e576c36c46f753d3356106267b83061ca5dbb6e
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-12-11 20:31:46 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -1,45 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
-class Main {
-    static int N, M;
-    static int[][] map;
-    static int ans = Integer.MAX_VALUE;
-    static int[] dir = {-1, 0, 1};
-    
-    public static void main(String[] args) throws IOException {
+public class Main {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
 
-        map = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+        int[][][] dp = new int[n][m][3];
+        for(int i = 0; i < n; i++) {
+            s = br.readLine().split(" ");
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(s[j]);
+                Arrays.fill(dp[i][j], (int)1e9);
             }
         }
         
-        for(int i = 0; i < M; i++){
-            dfs(0, i, -1, map[0][i]);
-        }
-
-        System.out.println(ans);
-    }
-    
-    public static void dfs(int r, int c, int predir, int tmpAns){
-        if(r == N-1){
-            ans = Math.min(ans, tmpAns);
-            return;
-        }
-        for(int i = 0; i < 3; i++){
-            int nc = c + dir[i];
-            if(nc >= 0 && nc < M && predir != i){
-                dfs(r+1, nc, i, tmpAns + map[r+1][nc]);
+        for(int j = 0; j < m; j++) {
+            for(int k = 0; k < 3; k++){
+                dp[0][j][k] = map[0][j];
             }
         }
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(j == 0) {
+                    dp[i][j][0] = Math.min(dp[i - 1][j + 1][1], dp[i - 1][j + 1][2]) + map[i][j];
+                    dp[i][j][1] = dp[i - 1][j][0] + map[i][j];
+                } else if(j == m - 1) {
+                    dp[i][j][2] = Math.min(dp[i - 1][j - 1][1], dp[i - 1][j - 1][0]) + map[i][j];
+                    dp[i][j][1] = dp[i - 1][j][2] + map[i][j];
+                } else {
+                    dp[i][j][0] = Math.min(dp[i - 1][j + 1][1], dp[i - 1][j + 1][2]) + map[i][j];
+                    dp[i][j][1] = Math.min(dp[i - 1][j][0], dp[i - 1][j][2]) + map[i][j];
+                    dp[i][j][2] = Math.min(dp[i - 1][j - 1][1], dp[i - 1][j - 1][0]) + map[i][j];
+                }
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int j = 0; j < m; j++) {
+            for (int i = 0; i < 3; i++) {
+                min = Math.min(min, dp[n - 1][j][i]);
+            }
+        }
+        System.out.println(min);
     }
 }
