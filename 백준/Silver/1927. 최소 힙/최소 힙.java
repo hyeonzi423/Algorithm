4af79bc9e7499edb1b4b5060c347e4350b1006d4
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2024-12-15 16:15:17 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 26640 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				if (pq.size() == 0) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.add(num);
+			}
+		}
+		System.out.println(sb);
+	}
+}
