2d73e6aa9af428368ad723188c5ece3b514b8f65
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-01-06 01:32:12 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1964 ms, Memory: 95360 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,67 +5,66 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-	static int N, M;
-	static int[][] map, record;
-	static boolean[][] visited;
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-	static Point start;
-
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		map = new int[N][M];
-		record = new int[N][M];
+		int[] goal = new int[3];
+
+		int[][] map = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == 2) {
-					start = new Point(i, j);
+					goal[0] = i;
+					goal[1] = j;
 				}
 			}
 		}
-		bfs();
+
+		boolean[][] visited = new boolean[N][M];
+		int[][] ans = new int[N][M];
+
+		int[] dx = { -1, 0, 1, 0 };
+		int[] dy = { 0, 1, 0, -1 };
+
+		Queue<int[]> q = new LinkedList<>();
+		q.add(goal);
+		visited[goal[0]][goal[1]] = true;
+
+		while (!q.isEmpty()) {
+			int[] now = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int nx = now[0] + dx[i];
+				int ny = now[1] + dy[i];
+				if (0 > nx || nx >= N || 0 > ny || ny >= M) {
+					continue;
+				}
+				if (visited[nx][ny]) {
+					continue;
+				}
+				if (map[nx][ny] == 0) {
+					continue;
+				}
+				q.add(new int[] { nx, ny, now[2] + 1 });
+				visited[nx][ny] = true;
+				ans[nx][ny] = now[2] + 1;
+			}
+		}
+
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if (map[i][j] == 1 && !visited[i][j]) {
 					System.out.print(-1 + " ");
 				} else {
-					System.out.print(record[i][j] + " ");
+					System.out.print(ans[i][j] + " ");
 				}
 			}
 			System.out.println();
 		}
 	}
-
-	public static void bfs() {
-		Queue<Point> q = new LinkedList<>();
-		q.add(start);
-		visited = new boolean[N][M];
-		visited[start.x][start.y] = true;
-
-		while (!q.isEmpty()) {
-			Point now = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-				if (inRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1) {
-					q.add(new Point(nx, ny));
-					record[nx][ny] = record[now.x][now.y] + 1;
-					visited[nx][ny] = true;
-				}
-			}
-		}
-
-	}
-
-	public static boolean inRange(int x, int y) {
-		return 0 <= x && x < N && 0 <= y && y < M;
-	}
-
 }
