aa67fcc040bb29efee3cceb8e68c4fcdeb4748e4
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-02-14 17:12:31 +0900

    [Gold V] Title: 감시 피하기, Time: 136 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -3,75 +3,67 @@ import java.io.*;
 
 class Main {
 
-    static int N, studentNum;
+    static int N;
     static char[][] map;
-    static ArrayList<int[]> teachers;
+    static List<int[]> teachers, spaces;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
         map = new char[N][N];
-        studentNum = 0;
         teachers = new ArrayList<>();
-        for(int i = 0; i < N; i++){
+        spaces = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
+            for (int j = 0; j < N; j++) {
                 map[i][j] = st.nextToken().charAt(0);
-                if(map[i][j] == 'S'){
-                    studentNum++;
-                }else if(map[i][j] == 'T'){
+                if (map[i][j] == 'T') {
                     teachers.add(new int[]{i, j});
+                } else if (map[i][j] == 'X') {
+                    spaces.add(new int[]{i, j});
                 }
             }
         }
 
-        combi(0);
-        
+        placeObstacles(0, 0);
+
         System.out.println("NO");
     }
 
-    public static void combi(int depth){
-        if(depth == 3){
-            int ret = simul();
-            if(ret == 0){
+    public static void placeObstacles(int depth, int start) {
+        if (depth == 3) {
+            if (simulate()) {
                 System.out.println("YES");
                 System.exit(0);
             }
             return;
         }
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(map[i][j] == 'X'){
-                    map[i][j] = 'O';
-                    combi(depth + 1);
-                    map[i][j] = 'X';
-                }
-            }
+        
+        for (int i = start; i < spaces.size(); i++) {
+            int[] pos = spaces.get(i);
+            map[pos[0]][pos[1]] = 'O';
+            placeObstacles(depth + 1, i + 1);
+            map[pos[0]][pos[1]] = 'X';
         }
     }
 
-    public static int simul(){
-        int cnt = 0;
-        for(int[] t : teachers){
-            for(int i = 0; i < 4; i++){
-                int nx = t[0] + dx[i];
-                int ny = t[1] + dy[i];
-                while(true){
-                    if(nx >= N || nx < 0 || ny >= N || ny < 0){
-                        break;
-                    }if(map[nx][ny] == 'O'){
-                        break;
-                    }if(map[nx][ny] == 'S'){
-                        cnt++;
-                    }
-                    nx += dx[i];
-                    ny += dy[i];
+    public static boolean simulate() {
+        for (int[] t : teachers) {
+            for (int d = 0; d < 4; d++) {
+                int nx = t[0] + dx[d];
+                int ny = t[1] + dy[d];
+                while (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                    if (map[nx][ny] == 'O') break;
+                    if (map[nx][ny] == 'S') return false;
+                    nx += dx[d];
+                    ny += dy[d];
                 }
             }
         }
-        return cnt;
+        return true;
     }
 }
