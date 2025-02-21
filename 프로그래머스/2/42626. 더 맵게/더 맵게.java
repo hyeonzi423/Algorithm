328d9e24f56a12382d23619ff41665576765c8d4
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-02-21 22:07:37 +0900

    [level 2] Title: 더 맵게, Time: 1528.64 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,17 +1,24 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int s : scoville){
             pq.add(s);
         }
-        while(pq.peek() < K && pq.size() >= 2){
-            int one = pq.poll();
-            int two = pq.poll();
-            pq.add(one + two*2);
+        
+        while(pq.size() >= 2){
+            int min = pq.poll();
+            if(min >= K){
+                break;
+            }
+            int nextMin = pq.poll();
+            pq.add(min + nextMin * 2);
             answer++;
         }
-        return pq.peek() >= K ? answer : -1;
+        
+        return pq.peek() < K ? -1 : answer;
     }
 }
