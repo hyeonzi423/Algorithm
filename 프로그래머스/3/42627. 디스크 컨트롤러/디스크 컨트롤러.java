53b94e5d64f9b20f5cbff544f764fc09228c6537
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-02-21 23:22:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.44 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,29 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0;
-        int time = 0;
-        PriorityQueue<int[]> remain = new PriorityQueue<>((p1, p2) -> Integer.compare(p1[0], p2[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> Integer.compare(p1[1], p2[1]));
-        for(int[] j : jobs){
-            remain.add(j);
-        }
-        while(pq.size() != 0 || remain.size() != 0){
-            while(remain.size() > 0){
-                if(remain.peek()[0] <= time){
-                    pq.add(remain.poll());
-                }else{
-                    break;
-                }
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+        int time = 0;  // 현재 시간
+        int total = 0; // 총 소요 시간 합
+        int idx = 0;   // jobs 배열 인덱스
+        int count = jobs.length;
+
+        while (idx < count || !pq.isEmpty()) {
+            while (idx < count && jobs[idx][0] <= time) {
+                pq.add(jobs[idx]);
+                idx++;
             }
-            if(pq.size() > 0){
-                int[] pop = pq.poll();
-                answer += time + pop[1] - pop[0];
-                time += pop[1];
-            }else{
-                time++;
+
+            if (!pq.isEmpty()) { 
+                int[] job = pq.poll();
+                time += job[1]; 
+                total += (time - job[0]); 
+            } else {
+                time = jobs[idx][0];
             }
         }
-        return answer/jobs.length; 
+
+        return total / count;
     }
 }
