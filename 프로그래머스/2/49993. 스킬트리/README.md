5aaad54e14b03684187e766f3ac3554649f8b2a2
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-03-04 14:18:29 +0900

    [level 2] Title: 스킬트리, Time: 10.08 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.23 ms
+메모리: 82.9 MB, 시간: 10.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 09일 09:17:31
+2025년 03월 04일 14:18:26
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,18 +1,17 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0; 
-        for (String one : skill_trees){
+        int answer = 0;
+        for(String s : skill_trees){
             StringBuilder check = new StringBuilder();
-            for(int i = 0; i < one.length(); i++){
-                Character c = one.charAt(i);
-                if(skill.contains(Character.toString(c))){
-                    check.append(c);
+            for(int i = 0; i < s.length(); i++){
+                if(skill.contains(s.charAt(i) + "")){
+                    check.append(s.charAt(i));
                 }
             }
             if(skill.startsWith(check.toString())){
                 answer++;
-            } 
-        }   
+            }
+        }
         return answer;
     }
 }
