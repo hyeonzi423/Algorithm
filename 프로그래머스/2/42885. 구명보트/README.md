fd1fb256be316bc2c150a4284b620dec41f5f1f9
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-03-07 21:25:39 +0900

    [level 2] Title: 구명보트, Time: 14.24 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 56.1 MB, 시간: 9.91 ms
+메모리: 74.4 MB, 시간: 14.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 12:47:30
+2025년 03월 07일 21:25:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,18 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int left = 0, right = people.length - 1;
         Arrays.sort(people);
         
+        int left = 0;
+        int right = people.length-1;
         while(left <= right){
             if(people[left] + people[right] <= limit){
-                left++;
-                right--;
-                answer++;
-            }else{
-                right--;
-                answer++;
+                left++;   
             }
+            right--;
+            answer++;
         }
         return answer;
     }
