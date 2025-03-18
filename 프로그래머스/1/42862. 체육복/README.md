89ea5aed162c98284cd911d4dd267bb14fa5238c
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-03-19 00:08:32 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 77.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 23:16:1
+2025년 03월 19일 00:08:27
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] count = new int[n+1];
+        
+        for(int l : lost){
+            count[l]--;
+        }
+        for(int r : reserve){
+            count[r]++;
+        }
+        
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (count[i] >= 0) {
+                answer++;
+            } else {
+                if (i > 1 && count[i - 1] == 1) {
+                    count[i - 1]--;
+                    answer++;
+                } else if (i < n && count[i + 1] == 1) {
+                    count[i + 1]--;
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
