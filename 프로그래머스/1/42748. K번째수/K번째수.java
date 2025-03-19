4e3c967b9b81e61f5f57564d860cbc144cc975a1
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-03-19 12:55:08 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int idx = 0;
+        
+        for(int[] c : commands){
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int i = c[0]-1; i < c[1]; i++){
+                pq.add(array[i]);
+            }
+            for(int i = 0; i < c[2] - 1; i++){
+                pq.poll();
+            }
+            answer[idx++] = pq.poll();
+        }
+        return answer;
+    }
+}
