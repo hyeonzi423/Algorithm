d62ae54c871bd4997603856e39263c74e84efa30
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-04-25 12:29:57 +0900

    [level 3] Title: 베스트앨범, Time: 1.57 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.62 ms
+메모리: 83.4 MB, 시간: 1.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 18:59:49
+2025년 04월 25일 12:29:54
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        
-        HashMap<String, ArrayList<int[]>> g = new HashMap<>(); // 장르별 인덱스와 플레이 수
-        HashMap<String, Integer> p = new HashMap<>(); // 장르별 총 합
+        Map<String, ArrayList<int[]>> gMap = new HashMap<>();
+        Map<String, Integer> cMap = new HashMap<>();
         
         for(int i = 0; i < genres.length; i++){
-            ArrayList<int[]> arr = g.getOrDefault(genres[i], new ArrayList<>());
-            arr.add(new int[]{plays[i], i});
-            g.put(genres[i], arr);
+            ArrayList<int[]> ret = gMap.getOrDefault(genres[i], new ArrayList<>());
+            ret.add(new int[]{i, plays[i]});
+            gMap.put(genres[i], ret);
             
-            p.put(genres[i], p.getOrDefault(genres[i], 0) + plays[i]);
+            int now = cMap.getOrDefault(genres[i], 0) + plays[i];
+            cMap.put(genres[i], now);
         }
         
-        List<String> sortedGenres = new ArrayList<>(p.keySet());
-        sortedGenres.sort((g1, g2) -> p.get(g2) - p.get(g1));
+        List<String> sorted = new ArrayList<>(cMap.keySet());
+        sorted.sort((g1, g2) -> cMap.get(g2) - cMap.get(g1));
         
         ArrayList<Integer> res = new ArrayList<>();
-        for(String genre : sortedGenres){
-            ArrayList<int[]> songs = g.get(genre);
-            songs.sort((s1, s2) -> s2[0] - s1[0]);
-        
-            for(int i = 0; i < Math.min(2, songs.size()); i++){
-                res.add(songs.get(i)[1]);
+        for(String s : sorted){
+            ArrayList<int[]> tmp = gMap.get(s);
+            tmp.sort((t1, t2) -> {
+                if(t1[1] != t2[1]) return t2[1] - t1[1];
+                return t1[0] - t2[0];
+            });
+            for(int i = 0; i < Math.min(2, tmp.size()); i++){
+                res.add(tmp.get(i)[0]);
             }
         }
         
