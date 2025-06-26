8628a6c5586f125d2641d2091f62f5512f9564c0
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-06-26 09:33:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[1001];
+        int N = Integer.parseInt(br.readLine());
 
+        int[] dp = new int[Math.max(N + 1, 3)]; // 최소 크기 3 확보
+        dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        if (N >= 2) {
+            dp[2] = 2;
+
+            for (int i = 3; i <= N; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[N]);
     }
 }
