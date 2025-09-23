4db28ac490e0e8f1f499b5e5a0f8d0dcd2652f76
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-09-23 20:46:58 +0900

    [Gold IV] Title: 최단경로, Time: 852 ms, Memory: 110256 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,73 +2,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
-class EndWeight{
-	int e, w;
-	
-	EndWeight(int e, int w) {
-		this.e = e;
-		this.w = w;
-	}
-}
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(br.readLine());
-		ArrayList<ArrayList<EndWeight>> lineInfo = new ArrayList<ArrayList<EndWeight>>();
 
-		
-		for(int i = 0; i <= V; i++) {
-			lineInfo.add(new ArrayList<>());
+		int V = Integer.parseInt(st.nextToken()); // 정점 수
+		int E = Integer.parseInt(st.nextToken()); // 간선 수
+		int K = Integer.parseInt(br.readLine()); // 시작 정점
+
+		ArrayList<int[]>[] edges = new ArrayList[V + 1];
+		for (int i = 0; i <= V; i++) {
+			edges[i] = new ArrayList<>();
 		}
-		
-		for(int i = 0; i< E; i++) {
+
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			lineInfo.get(start).add(new EndWeight(end, weight));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			edges[u].add(new int[] { v, w });
 		}
-		
-		boolean[] visited = new boolean[V+1];
-		int[] cost = new int[V+1];
-		for(int i = 1; i <= V; i++) {
-			cost[i] = Integer.MAX_VALUE;
-		}
-		
-		cost[K] = 0;
-		
-		for(int i = 0; i < V; i++) {
-			int maxValue = Integer.MAX_VALUE;
-			int idx = 0;
-			for(int j = 1; j <= V; j++) {
-				if (!visited[j] && cost[j] < maxValue) {
-					maxValue = cost[j];
-					idx = j;
-				}
-			}
-			visited[idx] = true;
-			for(int j = 0; j < lineInfo.get(idx).size(); j++) {
-				EndWeight now = lineInfo.get(idx).get(j);
-				if(cost[now.e] > cost[idx] + now.w) {
-					cost[now.e] = cost[idx] + now.w;
+
+		int[] dist = new int[V + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[K] = 0;
+
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+			return o1[0] - o2[0];
+		});
+		pq.add(new int[] { 0, K });
+
+		while (!pq.isEmpty()) {
+			int[] now = pq.poll();
+			if (now[0] != dist[now[1]]) continue; // 현재 최단거리가 아니라면 넘어감
+
+			for (int[] next : edges[now[1]]) {
+				int newDist = now[0] + next[1];
+				if (newDist < dist[next[0]]) {
+					dist[next[0]] = newDist;
+					pq.add(new int[] { newDist, next[0] });
 				}
 			}
 		}
-		
-		for(int i= 1; i<= V; i++) {
-			if(cost[i] < Integer.MAX_VALUE) {
-				System.out.println(cost[i]);
-			}
-			else {
-				System.out.println("INF");
-			}
+
+		for (int i = 1; i <= V; i++) {
+			System.out.println(dist[i] != Integer.MAX_VALUE ? dist[i] : "INF");
 		}
 	}
 }
