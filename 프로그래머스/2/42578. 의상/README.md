8b6bebf7cc38f2e3ecf50bca01eb4cfc206076d0
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-10-02 23:31:47 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.28 ms
+메모리: 81.2 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 08일 15:49:42
+2025년 10월 02일 23:31:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,13 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
         HashMap<String, Integer> clo = new HashMap<>();
-        for(String[] c: clothes){
+        
+        for(String[] c : clothes){
             clo.put(c[1], clo.getOrDefault(c[1], 0) + 1);
         }
-        for(int x : clo.values()){
-            answer *= (x + 1);
+        
+        int answer = 1;
+        for(String key : clo.keySet()){
+            answer *= (clo.get(key) + 1);
         }
         return answer - 1;
     }
