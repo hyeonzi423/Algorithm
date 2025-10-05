8af44d5e2aacd59e853f58a52a268c97cfeefc42
hyeonzi423 <116867636+hyeonzi423@users.noreply.github.com>
2025-10-05 22:53:47 +0900

    [level 2] Title: 더 맵게, Time: 1312.92 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 1520.92 ms
+메모리: 129 MB, 시간: 1312.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 16:48:52
+2025년 10월 05일 22:53:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,25 +1,19 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
         for(int s : scoville){
             pq.add(s);
         }
         
-        while(pq.size() >= 2){
-            if(pq.peek() >= K){
-                break;
-            }
-                
-            int one = pq.poll();
-            int two = pq.poll();
-            pq.add(one + two*2);
+        int answer = 0;
+        while(pq.size() >= 2 && pq.peek() < K){
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a + b*2);
             answer++;
         }
-    
+        
         return pq.peek() < K ? -1 : answer;
     }
 }
